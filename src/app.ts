22d08d66058f9de3cb228862c9e6bf61645ecb7b
vladislav <bincha.1997@gmail.com>
2020-04-24 23:07:54 +0900

    feat: added home page

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+
+import router from './routes';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
 
 export default app;
